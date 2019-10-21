c93f046e3f232ffed11dd7329025717b89f4df4c
policarpio <edlan.policarpio@mnfgroup.limited>
2019-10-21 20:24:06 +1100

    Assignment review: Missed id Added getters and setters in id

--- src/main/java/policarpio/edlan/recipe/model/UnitOfMeasure.java
+++ src/main/java/policarpio/edlan/recipe/model/UnitOfMeasure.java
@@ -1,10 +1,24 @@
 package policarpio.edlan.recipe.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
     public String unit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getUnit() {
         return unit;
